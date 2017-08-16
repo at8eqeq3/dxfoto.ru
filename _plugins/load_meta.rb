00830f557e6543248a8b2e8f53df9dd1ddec34f0
Dmitry Grigoriev <at8eqeq3@yandex.ru>
2017-08-16 13:18:47 +0300

    [skip ci] Disabling home-built S3 downloader

--- _plugins/load_meta.rb
+++ _plugins/load_meta.rb
@@ -1,18 +1,18 @@
-require 'aws-sdk'
-require 'fileutils'
-Jekyll::Hooks.register :site, :after_reset do |site|
-  unless Dir.entries(site.config['source'] + '/_data/').include? 'tags'
-    Aws.config.update region: 'eu-west-1',
-                      credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-    s3 = Aws::S3::Client.new
-    resp = s3.list_objects(bucket: 'meta-dxfoto-ru', 'max-keys': 4000)
-    resp.contents.each do |m|
-      next unless (m.key.start_with?('tags') || m.key.start_with?('exif') || m.key.start_with?('colors')) && m.key.end_with?('.json')
-      d = site.config['source'] + '/_data/' + m.key.split('/')[0..-2].join('/')
-      FileUtils.mkdir_p d
-      s3.get_object response_target: site.config['source'] + '/_data/' + m.key,
-                    bucket: 'meta-dxfoto-ru',
-                    key: m.key
-    end
-  end
-end
+#require 'aws-sdk'
+#require 'fileutils'
+#Jekyll::Hooks.register :site, :after_reset do |site|
+#  unless Dir.entries(site.config['source'] + '/_data/').include? 'tags'
+#    Aws.config.update region: 'eu-west-1',
+#                      credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+#    s3 = Aws::S3::Client.new
+#    resp = s3.list_objects(bucket: 'meta-dxfoto-ru')
+#    resp.contents.each do |m|
+#      next unless (m.key.start_with?('tags') || m.key.start_with?('exif') || m.key.start_with?('colors')) && m.key.end_with?('.json')
+#      d = site.config['source'] + '/_data/' + m.key.split('/')[0..-2].join('/')
+#      FileUtils.mkdir_p d
+#      s3.get_object response_target: site.config['source'] + '/_data/' + m.key,
+#                    bucket: 'meta-dxfoto-ru',
+#                    key: m.key
+#    end
+#  end
+#end
