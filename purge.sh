9d99e6cf34ab492f91ee0ff4f9d858d6f82b7d9a
Dmitry Grigoriev <dmitry@dxfoto.ru>
2018-09-16 19:30:37 +0300

    Fixing Travis CI config

---
+++ purge.sh
@@ -0,0 +1,1 @@
+curl -X POST "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/purge_cache" -H "X-Auth-Email: $CF_AUTH_EMAIL" -H "X-Auth-Key: $CF_API_KEY" -H "Content-Type: application/json" --data '{"files": ["https://www.dxfoto.ru/", "https://amp.dxfoto.ru/"]}'
