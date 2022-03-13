4ac7bde2d4df511099c9c2d9914c0976e980b295
at8eqeq3 <at8eqeq3@yandex.ru>
2022-03-13 10:01:12 +0300

    Fixed today redir API call

--- _plugins/today_generator.rb
+++ _plugins/today_generator.rb
@@ -4,8 +4,29 @@ module Jekyll
     safe true
     priority :low
     def generate(site)
-      today = File.new("#{__dir__}/../today", 'w')
-      today.puts site.posts.docs[-1].data['date'].strftime('https://img.dxfoto.ru/l/%Y/%m/%Y-%m-%d.jpg')
+      today = File.new("#{__dir__}/../today.json", 'w')
+      date = site.posts.docs[-1].data['date'].strftime('%Y/%m/%Y-%m-%d')
+      data = <<-TODAY
+<WebsiteConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+  <IndexDocument>
+    <Suffix>index.html</Suffix>
+  </IndexDocument>
+  <ErrorDocument>
+    <Key>error.html</Key>
+  </ErrorDocument>
+  <RoutingRules>
+    <RoutingRule>
+      <Condition>
+        <KeyPrefixEquals>today.jpg</KeyPrefixEquals>
+      </Condition>
+      <Redirect>
+        <ReplaceKeyPrefixWith>l/#{date}.jpg</ReplaceKeyPrefixWith>
+      </Redirect>
+    </RoutingRule>
+  </RoutingRules>
+</WebsiteConfiguration>
+      TODAY
+      today.puts data
     end
   end
 end
